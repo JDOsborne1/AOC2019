c057a46f43eef6a89b0eb1db39d0bfe9e84e0245
Joseph Osborne <Joseph.Osborne@edit.co.uk>
2019-12-11 16:31:30 +0000

    Cleaning up Vignette

---
+++ R/Day_10.R
@@ -0,0 +1,43 @@
+day10InputParser <- function(input_chars){
+
+        input_chars_parsed <- input_chars%>%
+                strsplit("\n") %>%
+                unlist() %>%
+                strsplit("")
+
+        input_matrix <- do.call(cbind, input_chars_parsed) %>% t()
+
+        which(input_matrix == "#", arr.ind = TRUE) %>%
+                as_tibble() %>%
+                mutate(coords = map2(col-1L, row-1L, c)) %>%
+                select(coords)
+
+}
+
+
+day10LengthCalc <- function(vect){
+        sqrt(sum(vect * vect)) * sign(vect[1])
+}
+day10MagnitudeCalc <- function(vect){
+        sqrt(sum(vect * vect))
+}
+day10AsteroidViewCount <- function(current_coord, asteroid_list){
+        asteroid_list %>%
+                mutate(diffs = map(coords, function(x) x - current_coord)) %>%
+                # mutate(diff_length = map(diffs, day10LengthCalc)) %>%
+                mutate(diff_mag = map(diffs, day10MagnitudeCalc)) %>%
+                filter(diff_mag != 0) %>%
+                mutate(norm_direction = map2(diffs, diff_mag, function(x, y) x/y)) %>%
+                mutate(
+                        norm_direction_x = map(norm_direction, function(x) round(x[1], 5))
+                        , norm_direction_y = map(norm_direction, function(x) round(x[2], 5))
+                ) %>%
+                unnest(cols = c(norm_direction_x, norm_direction_y, diff_mag)) %>%
+                arrange(diff_mag) %>%
+                distinct(
+                        norm_direction_x
+                        , norm_direction_y
+                        , .keep_all = T
+                ) %>%
+                nrow()
+}
